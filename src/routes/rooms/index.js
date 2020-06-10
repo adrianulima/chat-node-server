97d35793ea10743864fcd79b9b11abd0fc962208
junilima <junim3571100>
2020-06-10 01:57:22 -0300

    Created Factory Function to generate DBManager and apply to GET /rooms and GET /rooms/:id

--- src/routes/rooms/index.js
+++ src/routes/rooms/index.js
@@ -1,19 +1,46 @@
 import { Router } from 'express'
+import { map } from 'lodash'
+import { DBManager } from '../../utils/db'
 
 const rooms = Router()
-
-const roomsList = {}
+const roomManager = DBManager()
 
 rooms.get('/', (req, res) => {
-  const total = Object.keys(roomsList).length
-  const { limit = total, offset = 0 } = req.query
+  const { offset, limit } = req.query
+  roomManager
+    .getAll(offset, limit)
+    .then((roomList) => {
+      roomList.list = map(roomList.list, (room) => {
+        room = Object.assign({}, room)
+        room.protected = !!room.password
+        delete room.password
+        return room
+      })
+      res.status(200).json(roomList)
+      return
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(error.status || 500).send({ error })
+      return
+    })
+})
 
-  res.status(200).json({
-    list: roomsList,
-    total,
-    limit,
-    offset,
-  })
+rooms.get('/:id', (req, res) => {
+  const { id } = req.params
+  roomManager
+    .get(id)
+    .then((room) => {
+      room = Object.assign({}, room)
+      room.protected = !!room.password
+      delete room.password
+      res.status(200).json(room)
+      return
+    })
+    .catch((error) => {
+      res.status(error.status || 500).send({ error })
+      return
+    })
 })
 
 export default rooms
