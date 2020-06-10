b44c1f10b83eb02a50a4ae8423f7e9e598470a17
Cleomir Ferreira Lima Junior <30278498+junilima@users.noreply.github.com>
2020-06-10 20:50:09 -0300

    Implemented DBManager (#1)
    * Replaced roomList from Array to Object
    
    * Created Factory Function to generate DBManager and apply to GET /rooms and GET /rooms/:id
    
    * Replaced Object.assign usage by spread operator
    
    * Removed methods console.log and unnecessary returns, Object.assign replaced by spread operator and replaced to "delete db[id]" instead of setting null to remove completaly a key from objectMap
    
    Co-authored-by: junilima <junim3571100>

--- src/routes/rooms/index.js
+++ src/routes/rooms/index.js
@@ -1,18 +1,41 @@
 import { Router } from 'express'
+import { map } from 'lodash'
+import { DBManager } from '../../utils/db'
 
 const rooms = Router()
-
-const roomsList = []
+const roomManager = DBManager()
 
 rooms.get('/', (req, res) => {
-  const { limit = roomsList.length, offset = 0 } = req.query
+  const { offset, limit } = req.query
+  roomManager
+    .getAll(offset, limit)
+    .then((roomList) => {
+      roomList.list = map(roomList.list, (room) => {
+        room = { ...room }
+        room.protected = !!room.password
+        delete room.password
+        return room
+      })
+      res.status(200).json(roomList)
+    })
+    .catch((error) => {
+      res.status(error.status || 500).send({ error })
+    })
+})
 
-  res.status(200).json({
-    list: roomsList,
-    total: roomsList.length,
-    limit,
-    offset,
-  })
+rooms.get('/:id', (req, res) => {
+  const { id } = req.params
+  roomManager
+    .get(id)
+    .then((room) => {
+      room = { ...room }
+      room.protected = !!room.password
+      delete room.password
+      res.status(200).json(room)
+    })
+    .catch((error) => {
+      res.status(error.status || 500).send({ error })
+    })
 })
 
 export default rooms
---
+++ src/utils/db.js
@@ -0,0 +1,61 @@
+import { uniqueId, values, sortBy } from 'lodash'
+
+export const DBManager = () => {
+  const db = {}
+
+  return {
+    get: (id) => {
+      return new Promise((resolve, reject) => {
+        if (db[id]) resolve(db[id])
+        else reject({ error: { status: 401, message: 'Object not found' } })
+      })
+    },
+
+    getAll: (offset, limit) => {
+      return new Promise((resolve, reject) => {
+        offset = parseInt(offset) || 0
+        limit = parseInt(limit) || Object.keys(db).length
+
+        const list = sortBy(values(db), 'timestamp').slice(
+          offset,
+          offset + limit
+        )
+
+        resolve({
+          list: list,
+          total: Object.keys(db).length,
+          limit,
+          offset,
+        })
+      })
+    },
+
+    insert: (room) => {
+      return new Promise((resolve, reject) => {
+        const id = uniqueId()
+        if (!db[id]) {
+          db[room.id] = { ...room, id }
+          resolve(db[room.id])
+        } else reject({ error: { status: 500, message: 'Database error' } })
+      })
+    },
+
+    update: (room) => {
+      return new Promise((resolve, reject) => {
+        if (!!db[room.id]) {
+          db[room.id] = room
+          resolve(room)
+        } else reject({ error: { status: 401, message: 'Object not found' } })
+      })
+    },
+
+    delete: (id) => {
+      return new Promise((resolve, reject) => {
+        if (!!db[id]) {
+          delete db[id]
+          resolve(id)
+        } else reject({ error: { status: 401, message: 'Object not found' } })
+      })
+    },
+  }
+}
