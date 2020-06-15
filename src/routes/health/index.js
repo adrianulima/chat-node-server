53317ba117759308c841add29a40dc1e551c231b
Marc Victor Passarelli de Carvalho <mvpc1895@gmail.com>
2020-06-15 00:38:32 -0300

    Implemented Health Check endpoint at path '/health (#7)

---
+++ src/routes/health/index.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import HttpStatus from 'http-status-codes'
+
+const health = Router()
+
+health.get('/', (req, res) => {
+  res.status(HttpStatus.OK).json({ uptime: process.uptime() })
+})
+
+export default health
