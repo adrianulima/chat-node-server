0849175985f6797bb86ba791f24cd579ca5fc955
Marc Victor Passarelli de Carvalho <m_vpc@hotmail.com>
2020-06-14 21:54:25 -0300

    Implemented Health Check endpoint at path '/health

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import cors from 'cors'
 import { json } from 'body-parser'
 import rooms from './rooms'
+import health from './health'
 
 const router = Router()
 router.use(cors())
@@ -9,5 +10,6 @@ router.use(json())
 
 // routes
 router.use('/rooms', rooms)
+router.use('/health', health)
 
 export default router
